f15d0e922d394b2d17abecda618da7c3fe659e82
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-02-09 11:56:44 +0530

    first commit

---
+++ s.js
@@ -0,0 +1,13 @@
+const wordele= document.getElementById("word");
+const wrongletters= document.getElementById("wrong-l");
+const playagainbtn= document.getElementById("playbutton");
+const popup= document.getElementById("pop_up_msg");
+const notification= document.getElementById("notify");
+const finalmessage= document.getElementById("finalmsg");
+const figureparts=document.querySelectorAll("fig-part");
+const words=[
+    "application",
+    "programming",
+    "interface",
+    "wizard",
+];
